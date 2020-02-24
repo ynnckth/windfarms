1bef818313718b7f7d22e0bb34cf4f9dfbb06cad
ynnckth <yannickstreit@hotmail.com>
2020-02-24 20:44:28 +0100

    Fix responsiveness

--- WindfarmDashboard/client/src/components/Header/Header.tsx
+++ WindfarmDashboard/client/src/components/Header/Header.tsx
@@ -37,28 +37,29 @@ const Header: React.FC<IProps> = (props: IProps) => {
   };
 
   return (
-    <div className="header-content">
-      <div>Windfarm Dashboard</div>
+    <div className="header">
+      <div className="left-header-content">
+        <div className="title">Windfarm Dashboard</div>
+        {windfarms &&
+        <FormControl>
+          {selectedWindfarm
+            ? <InputLabel className={classes.select} id="select-label">Windfarm</InputLabel>
+            : <InputLabel className={classes.select} id="select-label">Select windfarm</InputLabel>}
 
-      {windfarms &&
-      <FormControl>
-        {selectedWindfarm
-          ? <InputLabel className={classes.select} id="select-label">Windfarm</InputLabel>
-          : <InputLabel className={classes.select} id="select-label">Select windfarm</InputLabel>}
-
-        <Select
-          className={classes.select}
-          style={{
-            minWidth: '100px',
-            maxWidth: '300px',
-            color: 'white',
-          }}
-          labelId="select-label"
-          value={selectedWindfarm?.id || ''}
-          onChange={onSelectedWindfarm}>
-          {windfarms.map(w => <MenuItem key={w.id} value={w.id}>{w.name}</MenuItem>)}
-        </Select>
-      </FormControl>}
+          <Select
+            className={classes.select}
+            style={{
+              minWidth: '100px',
+              maxWidth: '300px',
+              color: 'white',
+            }}
+            labelId="select-label"
+            value={selectedWindfarm?.id || ''}
+            onChange={onSelectedWindfarm}>
+            {windfarms.map(w => <MenuItem key={w.id} value={w.id}>{w.name}</MenuItem>)}
+          </Select>
+        </FormControl>}
+      </div>
       <img src={logo} className="logo" alt="Logo"/>
     </div>
   );
